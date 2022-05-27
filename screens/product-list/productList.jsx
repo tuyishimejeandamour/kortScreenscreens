955b9248727290602b93b2e983f359b48ccac551
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2022-05-27 08:32:34 +0200

    adding elements

---
+++ screens/product-list/productList.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './productList.styles'
+
+const ProductList = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ProductList</Text>
+    </View>
+  )
+}
+
+export default ProductList
